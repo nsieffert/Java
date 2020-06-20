5abf25c926ce08983754dd5aa0267fdb2c2b26c7
Norita Sieffert <nsieffert@tkocustoms.com>
2020-06-20 15:54:14 -0500

    Banking Udemy Exercise

---
+++ exercise2/Main.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
--- udemy_exercises/bank/Customer.java
+++ udemy_exercises/bank/Customer.java
@@ -1,4 +1,26 @@
 package bank;
 
+import java.util.ArrayList;
+//Using autoboxing to create the array list - Double
+
 public class Customer {
+    private String customerName;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialAmount) {
+        this.customerName = name;
+        this.transactions = new ArrayList<Double>();
+        addTransaction(initialAmount);
+    }
+    public void addTransaction(double amount){
+        this.transactions.add(amount);
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
 }
